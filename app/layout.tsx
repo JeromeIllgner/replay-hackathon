2f97af279b5c07ae9981266a20f48a6ab84b3438
Jerome Illgner <j.illgner@outlook.com>
2025-03-05 00:08:40 +0000

    Fixed deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { getBids } from "./actions";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
